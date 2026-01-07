d48f3ef918bc2332e363665c5e84528db5bd7797
nhlo852 <m283852@usna.edu>
2026-01-07 15:11:24 -0500

    msg

---
+++ lab01/lab01.cpp
@@ -0,0 +1,19 @@
+//Nathan Lo 283852
+//Lab 01 Final Submission :)
+#include <iostream>
+using namespace std;
+int main()
+{
+  cout << "Hello World!" << endl;
+  cout << "Hello Solar System!" << endl;
+  cout << "Hello Galaxy!" << endl;
+  cout << "Hello Universe!" << endl;
+  cout << "Hello ... ?" << endl;
+  cout << "This is a 1!" << endl;
+  cout << "This is a 2!" << endl;
+  cout << "This is a 3!" << endl;
+  cout << "This is a 4!" << endl;
+  cout << "This is a 5!" << endl;
+  
+  return 0;
+}
