254b55a97a871fc82d4b77571c3a29560af9fad4
nhlo852 <m283852@usna.edu>
2026-01-08 18:59:50 -0500

    msg

---
+++ hw02/hw.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  // Take inputs
+  int feet1, feet2;
+  int inches1, inches2;
+  char junk;
+  cout << "Enter two lengths in feet and inches (larger first!)";
+  cin >> feet1 >> junk;
+  cin >> inches1 >> junk >> junk;
+  cin >> feet2 >> junk; 
+  cin >> inches2 >> junk >> junk;
+
+  // Convert feet to inches
+  int totalInches1 = (feet1 * 12) + inches1;
+  int totalInches2 = (feet2 * 12) + inches2;
+
+  //Subtract and convert back
+  int totalInches3 = totalInches1 - totalInches2;
+  int finalFeet = totalInches3/12;
+  int finalInches = totalInches3%12;
+
+  
+  cout << "Difference is "<< finalFeet <<"' "<<finalInches<<"''";
+  
+  
+  return 0;
+}
