74377e371677b72323a67cd4ae93939373e2e2e5
nhlo852 <m283852@usna.edu>
2026-01-16 14:53:08 -0500

    msg

--- hw06/hw.cpp
+++ hw06/hw.cpp
@@ -1,38 +1,40 @@
 //Nathan Lo m283852
-//Letter Identifier
+//Add or Square
 
 #include <iostream>
 #include <string>
-#include <math>
+
 using namespace std;
 
 int main()
 {
   double a ,b;
+  char comma;
   string act;
-  cout << "Values for a and b?";
-  cin >> a >> b;
-  cout << "What do you want?";
+  cout << "Values for a and b? ";
+  cin >> a >> comma >> b;
+  cout << "What do you want? ";
   cin >> act;
 
   //Add
   if (act == "add") {
     string junk, who;
     double add; 
-    cin >> add >> string >> who;
+    cin >> add >> junk >> who;
+
     //Who we adding to?
     if (who == "a") {
-        cout << "a = " << a + sum << " and " << "b = " << b << endl;
+        cout << "a = " << a + add << " and " << "b = " << b << endl;
     }
     if (who == "b") {
-        cout << "a = " <<  << " and " << "b = " << b << endl;
+        cout << "a = " << a << " and " << "b = " << b + add << endl;
     }
   }
   //Square
   if (act == "square") {
     string who;
-    cout << square << who <<endl;
-    double square;
+    cin >> who;
+
     //Who we squaring?
     if (who == "a") {
         cout << "a = " << a*a << " and " << "b = " << b << endl;
