d018ea216ffa5554bf71efdc3d8fb1c406406f75
nhlo852 <m283852@usna.edu>
2026-03-19 15:14:01 -0400

    msg

--- lab09/p1.cpp
+++ lab09/p1.cpp
@@ -13,8 +13,8 @@
 using namespace std;
 int main()
 {
-    double rating, total = 0;
-    int num, userID, movieId, count = 0;
+    double rating;
+    int num, userID, movieId;
     string junk1, junk2, junk3, act = "o", prog = "huh";
     ifstream fin("ratings.tsv");
     double** R = new double*[610];
@@ -31,24 +31,25 @@ int main()
         R[userID][movieId] = rating;
 
     }
-    while (act != "quit") {
-        cin >> act >> num;
+    while (cin >> act && act != "quit") {
+        double total = 0;
+        int count = 0;
+        cin >>  num;
         if (act == "movie") {
             for (int i = 0; i < 9724; i++) {
-                total += R[num][i]; 
+                total += R[i][num]; 
                 count ++;
             }
             cout << total/count;
         }
         if (act == "user") {
             for (int i = 0; i < 610; i++) {
-                total += R[i][num]; 
+                total += R[num][i]; 
                 count ++;
             }
             cout << total/count;
 
         }
-        else break;
     }
     fin.close();
     for (int i = 0; i < 9724; i++) {
